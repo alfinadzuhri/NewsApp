13b2c31eb577766ac725c88f901ed875abfaa2c4
Alfin Adzuhri Delangga Putra <zurapro21@gmail.com>
2024-06-29 13:15:04 +0700

    Initial Commit

---
+++ test02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test02
+//
+//  Created by KyMo Channel on 29/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
